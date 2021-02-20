fb670af9f0a1e0f75043c76ca04419df51e4413f
Hadi Hassan <hadihassan09@gmail.com>
2021-02-20 22:57:02 +0200

    added login handeling fixed user not being redirected upon failed request added loading when application is still in loading faze

--- App.tsx
+++ App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import LoginScreen from './app/screens/LoginScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import AsyncStorage from '@react-native-community/async-storage';
-import { AuthContext } from './app/models/context';
+import {AuthContext} from './app/models/context';
+import axios from 'axios';
+import {View} from 'react-native';
+import {ActivityIndicator} from 'react-native';
+import colors from './app/styles/colors';
 
 const Stack = createStackNavigator();
 
 interface LoginState {
-  isLoading: boolean,
-  token: string|null,
-  id: string|null,
-  isLoggedIn: boolean
-};
+  isLoading: boolean;
+  token: string | null;
+  isLoggedIn: boolean;
+}
 
 const App = () => {
-
   const initialLoginState: LoginState = {
     isLoading: true,
     token: null,
-    id: null,
-    isLoggedIn: false
+    isLoggedIn: false,
   };
 
   //Auth Actions
@@ -30,26 +31,22 @@ const App = () => {
         return {
           ...prevState,
           token: action.token,
-          id: action.id,
           isLoading: false,
-          isLoggedIn: action.isLoggedIn
+          isLoggedIn: action.isLoggedIn,
         };
       case 'LOGIN':
         return {
           ...prevState,
           token: action.token,
-          id: action.id,
           isLoading: false,
-          isLoggedIn: true
+          isLoggedIn: true,
         };
       case 'LOGOUT':
         return {
           ...prevState,
           token: '',
-          type: null,
-          id: null,
           isLoading: false,
-          isLoggedIn: false
+          isLoggedIn: false,
         };
       case 'UPDATE_PROFILE':
         return {
@@ -59,63 +56,101 @@ const App = () => {
     }
   };
 
-  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+  const [loginState, dispatch] = React.useReducer(
+    loginReducer,
+    initialLoginState,
+  );
 
   //Auth Context
-  const authContext = React.useMemo(() => ({
-    signIn: async (token: string, id: string) => {
-      try {
-        await AsyncStorage.setItem('token', token);
-        await AsyncStorage.setItem('id', id);
-      } catch (e) {
-        console.log(e);
-      }
-      dispatch({ action: 'LOGIN', token: token, id: id });
-    },
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async (token: string) => {
+        try {
+          await AsyncStorage.setItem('token', token);
+        } catch (e) {
+          console.log(e);
+        }
+        dispatch({action: 'LOGIN', token: token});
+      },
 
-    signOut: async () => {
-      try {
-        await AsyncStorage.removeItem('token');
-        await AsyncStorage.removeItem('id')
-      } catch (e) {
-        console.log(e);
-      }
-      dispatch({ action: 'LOGOUT' });
-    },
+      signOut: async () => {
+        try {
+          await AsyncStorage.removeItem('token');
+        } catch (e) {
+          console.log(e);
+        }
+        dispatch({action: 'LOGOUT'});
+      },
 
-    data: () => { return loginState }
-
-  }), [loginState]);
+      data: () => {
+        return loginState;
+      },
+    }),
+    [loginState],
+  );
 
   React.useEffect(() => {
     setTimeout(async () => {
       let isLoggedIn = false;
-      let token: string|null = '';
-      let id: string|null = '';
+      let token: string | null = '';
       try {
-        id = await AsyncStorage.getItem('id');
         token = await AsyncStorage.getItem('token');
-      dispatch({action: 'RETRIEVE_TOKEN', token: token,isLoggedIn: isLoggedIn, id: id});
-    } catch (e) {
-      console.log(e);
-    }}, 1500);
+        if (token !== '' && token != null) isLoggedIn = true;
+        dispatch({
+          action: 'RETRIEVE_TOKEN',
+          token: token,
+          isLoggedIn: isLoggedIn,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }, 1500);
   }, []);
 
-  return (
-    <AuthContext.Provider value={authContext}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen 
-            name="Login" 
-            component={LoginScreen}
-            options={{
-              headerShown: false
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthContext.Provider>
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async function (error) {
+      if (error.response.status === 401) {
+        authContext.signOut();
+      }
+      return Promise.reject(error);
+    },
   );
+
+  axios.defaults.baseURL = 'http://192.168.1.108:8000/api';
+  axios.defaults.headers.common['Authorization'] =
+    'Bearer' + ' ' + loginState.token;
+  axios.defaults.headers.post['Content-Type'] =
+    'application/json; charset=UTF-8';
+
+  if (loginState.isLoading == true)
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" color={colors.GREEN} />
+      </View>
+    );
+  if (loginState.isLoading == false)
+    return (
+      <AuthContext.Provider value={authContext}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            {loginState.isLoggedIn === true ? (
+              <Stack.Screen name="Login" component={LoginScreen} options={{}} />
+            ) : (
+              <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthContext.Provider>
+    );
 };
 
 export default App;
