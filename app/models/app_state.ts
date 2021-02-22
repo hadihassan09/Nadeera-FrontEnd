4c7a30dc185edd35321057023704d443e4ac99c2
Hadi Hassan <hadihassan09@gmail.com>
2021-02-22 14:30:58 +0200

    added AddImage and GetImages api methods

--- app/models/app_state.ts
+++ app/models/app_state.ts
@@ -22,3 +22,33 @@ export const login = async (data: any, onSuccess: any, onFailure: any) => {
             onFailure({"error":err.response.data, "status": err.response.status});
     });}
 };
+
+export const addImage = async(data: any, onSuccess: any, onFailure: any) => {
+    /* @ts-ignore */
+    if (internetStatus === false)
+        onFailure({
+            "error":"Connection Error"
+        });
+    else
+    {    
+        axios.post('/image', data).then(response => {
+        onSuccess(response.data);
+    }).catch(err => {
+            onFailure({"error":err.response.data, "status": err.response.status});
+    });}
+};
+
+export const getImages = async(onSuccess: any, onFailure: any) => {
+    /* @ts-ignore */
+    if (internetStatus === false)
+        onFailure({
+            "error":"Connection Error"
+        });
+    else
+    {    
+        axios.get('/image/all').then(response => {
+        onSuccess(response.data);
+    }).catch(err => {
+            onFailure({"error":err.response.data, "status": err.response.status});
+    });}
+};
